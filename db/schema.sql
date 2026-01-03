af4ac78b57142580dfd6e493205621b7a1190c86
NicolasSeivane <seivanenicolas@gmail.com>
2026-01-03 16:00:15 -0300

    Cleaning repo to run once again

--- db/schema.sql
+++
@@ -1,3 +0,0 @@
-CREATE TABLE IF NOT EXISTS scoring_dataset (age INTEGER, sex TEXT, bmi FLOAT, children INTEGER, smoker TEXT, region TEXT, charges FLOAT);
-
-CREATE TABLE IF NOT EXISTS training_dataset (age INTEGER, sex TEXT, bmi FLOAT, children INTEGER, smoker TEXT, region TEXT, charges FLOAT);
